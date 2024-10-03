5f409d07bcbbbaaeb3c14e018d60b41b9bd0ae69
ChanHyuc <whk1140@gmail.com>
2024-10-03 20:41:18 +0900

    Init: BankManager 프로젝트 생성

---
+++ BankManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BankManager
+//
+//  Created by ChanHyuc on 10/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
+    private func configureUI() {
+        view.backgroundColor = .systemBackground
+    }
+}
