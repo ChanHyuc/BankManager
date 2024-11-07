fdb66479b7dc65bc0aa48f73b7bf7be7c4e2b488
ChanHyuc <whk1140@gmail.com>
2024-11-07 20:33:36 +0900

    Test: test for Dependency Injection (DI) in CustomerCount

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Customer.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Customer.swift
@@ -1,9 +1,16 @@
-struct Customer {
+struct Customer: CustomerCountProtocol {
+    func createRandomCustomerCount() -> Int {
+        return customerCount.createRandomCustomerCount()
+    }
+    
+    var customerCount: CustomerCountProtocol
     let waitingNumber: String
     var business: BusinessType?
     
-    init(waitingNumber: String, business: BusinessType? = nil) {
+    init(customerCount: CustomerCountProtocol, waitingNumber: String, business: BusinessType? = nil) {
+        self.customerCount = customerCount
         self.waitingNumber = waitingNumber
         self.business = business
     }
+    
 }
--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -1,6 +1,10 @@
 import Foundation
 
-final class MainConsole {
+final class MainConsole: CustomerCountProtocol {
+    func createRandomCustomerCount() -> Int {
+        return CustomerCount().createRandomCustomerCount()
+    }
+    
     var linkedList: LinkedList = LinkedList<Any>()
     var serviceTime: Double = 0.0
     
@@ -10,7 +14,7 @@ final class MainConsole {
             let input = readLine()!
             
                 if input == "1" {
-                    for num in 1...self.customerCount {
+                    for num in 1...createRandomCustomerCount() {
                         self.linkedList.enqueue(data: num)
                     }
                     
@@ -30,7 +34,7 @@ final class MainConsole {
     private func processCustomers() {
         let delay = 0.7
         
-        for _ in 1...customerCount {
+        for _ in 1...createRandomCustomerCount() {
             if let customer = linkedList.dequeue() {
                 serviceTime += delay
                 
@@ -42,7 +46,7 @@ final class MainConsole {
         }
         
         DispatchQueue.global().asyncAfter(deadline: .now() + serviceTime) {
-            print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(self.customerCount)명이며, 총 업무시간은 \(String(format: "%.2f", self.serviceTime)) 초 입니다.")
+            print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(self.createRandomCustomerCount())명이며, 총 업무시간은 \(String(format: "%.2f", self.serviceTime)) 초 입니다.")
         }
     }
     
